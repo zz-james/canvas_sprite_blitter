4adddb0e527bdd3732fc7a635fbcd21608f5c571
zz-james <zz.james@gmail.com>
2013-08-23 23:10:13 +0100

    starting to implement penguin warrior

--- warrior.js
+++ warrior.js
@@ -1,5 +1,5 @@
 /**
- * Created with JetBrains WebStorm.
+ * Project: canvas_sprite_blitter
  * User: james
  * Date: 22/08/13
  * Time: 21:49
@@ -16,73 +16,44 @@ var num_star_tiles;   /* derived from the width of the loaded strips */
  * Loads the game's resources. Exits the program on failure.
  */
 var LoadGameData = function() {
-    SDL_Surface *tmp;
+    var tmp= PIX.SURF_NewSurface();
 
     /* The player's ship is stored as a 8640x96 image.
      This strip contains 90 individual images of the ship, rotated in
      four-degree increments. Take a look at fighter.bmp in an image
      viewer to see exactly what this means. */
-    tmp = SDL_LoadBMP("fighter.bmp");
-    if (tmp == NULL) {
-        fprintf(stderr, "Unable to load ship image: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE); // PFJ - changed for portable exit
-    }
+     PIX.IMG_QueueImage(ship_strip,"fighter.bmp");
 
-    /* Set the color key on the ship animation strip to black.
+     /* Set the color key on the ship animation strip to black.
      Enable RLE acceleration for a performance boost. */
-    SDL_SetColorKey(tmp, SDL_SRCCOLORKEY|SDL_RLEACCEL, 0);
+     //SDL_SetColorKey(tmp, SDL_SRCCOLORKEY|SDL_RLEACCEL, 0);
 
-    /* Optimize the entire strip for fast display. */
-    ship_strip = SDL_DisplayFormat(tmp);
-    if (ship_strip == NULL) {
-        /* We were unable to convert the image (for some foul reason),
-         but we can still use the copy we already have. This will
-         involve a performance loss. However, this should never happen. */
-        ship_strip = tmp;
-    } else SDL_FreeSurface(tmp);
-
-    /* Now load the star tiles. Each tile is 64x64, assembled into a strip.
+     /* Now load the star tiles. Each tile is 64x64, assembled into a strip.
      We'll derive the number of tiles from the width of the loaded bitmap. */
-    tmp = SDL_LoadBMP("back-stars.bmp");
-    if (tmp == NULL) {
-        fprintf(stderr, "Unable to load background tiles: %s\n",
-            SDL_GetError());
-        exit(EXIT_FAILURE); // PFJ - changed for portable exit
-    }
+     PIX.IMG_QueueImage(back_star_tiles,"back-stars.bmp");
 
-    /* Determine how many star tiles are in the strip. We'll assume that the
+     /* Load the front (parallaxing) set of star tiles. */
+     PIX.IMG_QueueImage(front_star_tiles,"front-stars.bmp");
+
+
+};
+
+var imagesLoaded = function() {
+
+   /* Determine how many star tiles are in the strip. We'll assume that the
      foreground and background strips contain the same number of stars. */
-    num_star_tiles = tmp->w / 64;
-    num_star_tiles = 4;
-
-    /* Attempt to optimize this strip for fast blitting. */
-    back_star_tiles = SDL_DisplayFormat(tmp);
-    if (back_star_tiles == NULL)
-        back_star_tiles = tmp;
-    else	SDL_FreeSurface(tmp);
-
-    /* Load the front (parallaxing) set of star tiles. */
-    tmp = SDL_LoadBMP("front-stars.bmp");
-    if (tmp == NULL) {
-        printf("Unable to load parallax tiles: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE); // PFJ - changed for portable exit
-    }
+    num_star_tiles = back_star_tiles.surface.width / 64;
+    //num_star_tiles = 4;
 
     /* Set a black color key and request RLE acceleration. */
-    SDL_SetColorKey(tmp, SDL_SRCCOLORKEY | SDL_RLEACCEL, 0);
+    //SDL_SetColorKey(tmp, SDL_SRCCOLORKEY | SDL_RLEACCEL, 0);
 
-    /* Attempt to optimize this strip for fast blitting. */
-    front_star_tiles = SDL_DisplayFormat(tmp);
-    if (front_star_tiles == NULL)
-        front_star_tiles = tmp;
-    else
-        SDL_FreeSurface(tmp);
 };
 
 /* ----------------------------------------------------------------------- */
 
 /* Data structure for player ships */
-function NewPlayer() {
+var NewPlayer = function() {
     var player_type = {
         angle:undefined,	  /* in clockwise degrees, 0 -> due east */
         world_x:undefined,    /* coordinates in the world (double) */
